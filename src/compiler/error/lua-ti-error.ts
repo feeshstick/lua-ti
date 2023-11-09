d232c36a5bb65ea83bd1fc979c3984d7fa25a4f2
lphs11 <lukas.haase@thm.mni.de>
2023-11-09 01:42:56 +0100

    add error class, differentiate between internal errors, syntax errors

---
+++ src/compiler/error/lua-ti-error.ts
@@ -0,0 +1,40 @@
+import {ExpressionContainer} from "../components/types.js";
+import {Variable} from "../table/symbol-table-2.js";
+import {IdentifierContainer} from "../components/nodes/expression/literal/identifier-container.js";
+
+export enum LuaTiErrorKind {
+    Internal,
+    Syntax
+}
+
+export class LuaTiError extends Error {
+    constructor(
+        private readonly _kind: LuaTiErrorKind,
+        private readonly _message: string
+    ) {
+        super();
+    }
+    static noEntry(target: ExpressionContainer): LuaTiError {
+        return new LuaTiError(LuaTiErrorKind.Internal, `tried to access undefined entry of expression $${target.id}\n${target.text}`)
+    }
+    
+    static illegalParameterDeclaration(): LuaTiError {
+        return new LuaTiError(LuaTiErrorKind.Internal, 'illegal parameter-declaration')
+    }
+    
+    static overwriteEntry(target: ExpressionContainer, value: ExpressionContainer): LuaTiError {
+        return new LuaTiError(LuaTiErrorKind.Internal, `entry of node $${target.id} would be overwritten by $${value.id}`)
+    }
+    
+    static overwriteEntryDirect(target: ExpressionContainer, value: Variable): LuaTiError {
+        return new LuaTiError(LuaTiErrorKind.Internal, `entry of node $${target.id} would be overwritten by ${value}`)
+    }
+    
+    static duplicateDeclaration(previous: Variable, current: Variable) {
+        return new LuaTiError(LuaTiErrorKind.Syntax, `previous=[${previous.declarations.map(x => x.text).join(',')}], current=[${current.declarations.map(x => x.text).join(', ')}]`)
+    }
+    
+    static cannotFindName(ident: IdentifierContainer) {
+        return new LuaTiError(LuaTiErrorKind.Syntax, `cannot find name '${ident.name}'`);
+    }
+}
