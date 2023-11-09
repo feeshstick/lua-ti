6df5c2eea5641a11d84e404316e206179968c05b
lphs11 <lukas.haase@thm.mni.de>
2023-11-09 01:41:37 +0100

    add new variable entry to expression-container

--- src/compiler/components/nodes/expression/abstract-expression-container.ts
+++ src/compiler/components/nodes/expression/abstract-expression-container.ts
@@ -1,6 +1,7 @@
 import {NodeKind} from "../../types.js";
 import {Type} from "../../../type/type-system.js";
 import {BaseContainer} from "../../base-container.js";
+import {Variable} from "../../../table/symbol-table-2.js";
 
 export type ExpressionContainerKind =
     | NodeKind.Identifier
@@ -22,4 +23,25 @@ export type ExpressionContainerKind =
 
 export abstract class AbstractExpressionContainer<E extends ExpressionContainerKind> extends BaseContainer<E> {
     _type: Type | undefined
+    __symbol: Variable | undefined
+    
+    getEntry<E extends Error>(err: E) {
+        if (!this.__symbol) {
+            throw err
+        } else {
+            return this.__symbol
+        }
+    }
+    
+    setEntry<E extends Error>(variable: Variable, err: E) {
+        if (this.__symbol) {
+            throw err
+        } else {
+            this.__symbol = variable
+        }
+    }
+    
+    hasEntry() {
+        return !!this.__symbol
+    }
 }
