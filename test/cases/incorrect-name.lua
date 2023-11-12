1342367c4f67362d4d0af28f0f920df235442e94
Hatter <sanity.sierota@protonmail.com>
2023-11-12 09:42:22 +0800

    use different script for incorrect-name

--- test/cases/incorrect-name.lua
+++ test/cases/incorrect-name.lua
@@ -1,19 +1,29 @@
+--テラ・フォーミング
+--Terraforming
 local s,id=GetID()
 function s.initial_effect(c)
+    --Activate
     local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
-function s.tgfilter(c)
-    return c:IsMonster() and c:IsAbleToGrave()
+function s.filter(c)
+    return c:IsType(TYPE_FIELD) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(0,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
-        --error here, SendToGrave should be SendtoGrave
-        Duel.SendToGrave(g,REASON_EFFECT)
+        --error here, SendToHand should be SendtoHand
+        Duel.SendToHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
 end
